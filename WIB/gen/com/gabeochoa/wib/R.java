fc6711afc01c046b6857dd1830ec03cbb994fc0f
Taylor Foxhall <lxmachina@gmail.com>
2014-02-15 14:28:54 -0500

    fix labeling

--- WIB/gen/com/gabeochoa/wib/R.java
+++ WIB/gen/com/gabeochoa/wib/R.java
@@ -84,7 +84,6 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f05000a;
         public static final int imgButtonFiveLabel=0x7f050006;
         public static final int imgButtonFourLabel=0x7f050005;
         public static final int imgButtonOneLabel=0x7f050002;
@@ -92,14 +91,14 @@ public final class R {
         public static final int imgButtonSixLabel=0x7f050007;
         public static final int imgButtonThreeLabel=0x7f050004;
         public static final int imgButtonTwoLabel=0x7f050003;
-        public static final int title_activity_about_view=0x7f05000c;
-        public static final int title_activity_calendar_view=0x7f05000f;
-        public static final int title_activity_facebook_view=0x7f05000b;
-        public static final int title_activity_members_view=0x7f05000e;
-        public static final int title_activity_past_events_view=0x7f050010;
-        public static final int title_activity_twitter_view=0x7f050011;
+        public static final int title_activity_about_view=0x7f05000b;
+        public static final int title_activity_calendar_view=0x7f05000e;
+        public static final int title_activity_facebook_view=0x7f05000a;
+        public static final int title_activity_members_view=0x7f05000d;
+        public static final int title_activity_past_events_view=0x7f05000f;
+        public static final int title_activity_twitter_view=0x7f050010;
         public static final int title_activity_web_view=0x7f050009;
-        public static final int title_activity_wib_app=0x7f05000d;
+        public static final int title_activity_wib_app=0x7f05000c;
     }
     public static final class style {
         /** 
