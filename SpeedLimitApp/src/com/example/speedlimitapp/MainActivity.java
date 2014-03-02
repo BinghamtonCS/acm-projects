0bebd351dce4d8294f77038d0b8d98b17d82dbb7
James <jedouard1994@gmail.com>
2014-03-02 11:46:09 -0500

    Current Speed Project
    -James Edouard

--- SpeedLimitApp/src/com/example/speedlimitapp/MainActivity.java
+++ SpeedLimitApp/src/com/example/speedlimitapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.speedlimitapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void startUserActivity(View v)
+	{
+		
+	}
+	
+	public void startMapActivity(View v)
+	{
+		Intent intent = new Intent(this, MapDataActivity.class);
+		startActivity(intent);
+	}
+	
+	public void startRoadActivity(View v)
+	{
+		
 	}
 
 }
---
+++ SpeedLimitApp/src/com/example/speedlimitapp/MapDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.speedlimitapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapDataActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.map_layout);
+	}
+
+}
